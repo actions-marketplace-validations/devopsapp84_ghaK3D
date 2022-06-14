1c6a94f306feea6742442b01e78d3d21992de497
Janusz <kujawa.j@pg.com>
2022-06-14 12:18:51 +0200

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # WHEN:        WHO:             WHAT:
 # 04/08/2022   Janusz Kujawa    Created initial parts of script.
-# 04/22/2022   Janusz Kujawa	Added invalid_option function
+# 04/22/2022   Janusz Kujawa	  Added invalid_option function
 # 14/06/2022   Janusz Kujawa    Adjust script to use within GH actions
 
 ### Variables
-clustername="${2:-"small"}"
-k3d_log="/tmp/$clustername-create.log"
+CLUSTER_NAME="${2:-"small"}"
+k3d_log="/tmp/$CLUSTER_NAME-create.log"
 
 usage() {                                     
    echo
@@ -37,9 +37,9 @@ progress_k3d_cluster() {
         sp=${sp#?}${sp%???}
       else
         if grep -q "deleted" "$k3d_log"; then
-          printf "%b" "\n\U2705 Cluster: \e[1;34m$clustername\e[0m sucessfully deleted\n"
+          printf "%b" "\n\U2705 Cluster: \e[1;34m$CLUSTER_NAME\e[0m sucessfully deleted\n"
         elif grep -q "created" "$k3d_log"; then
-          printf "%b" "\n\U2705 Cluster: \e[1;34m$clustername\e[0m sucessfully created\n"
+          printf "%b" "\n\U2705 Cluster: \e[1;34m$CLUSTER_NAME\e[0m sucessfully created\n"
         else
           printf "%b" "\n\U26A1 Unsupported!\n"
         fi
@@ -52,38 +52,38 @@ progress_k3d_cluster() {
 
 function create_k3d_cluster {
   ### Create cluster if des not exist
-  if [[ $(k3d cluster list --no-headers | grep $clustername) ]]; then
-    printf "%b" "\U26D4 Cluster: \e[1;34m$clustername\e[0m already exist!\n"
+  if [[ $(k3d cluster list --no-headers | grep $CLUSTER_NAME) ]]; then
+    printf "%b" "\U26D4 Cluster: \e[1;34m$CLUSTER_NAME\e[0m already exist!\n"
     exit
-  elif [ -f "./k3d/${clustername}.conf" ]; then
-    printf "%b" "\U1F525 Creating Kubernetes Cluster: \e[1;34m$clustername\e[0m "
-    k3d cluster create ${clustername} -c ./conf/${clustername}.yaml >> $k3d_log &
+  elif [ -f "./k3d/${CLUSTER_NAME}.conf" ]; then
+    printf "%b" "\U1F525 Creating Kubernetes Cluster: \e[1;34m$CLUSTER_NAME\e[0m "
+    k3d cluster create ${CLUSTER_NAME} -c ./conf/${CLUSTER_NAME}.yaml >> $k3d_log &
   else
-    printf "%b" "\U1F440 Kubernetes cluster config: \e[1;34m${clustername}.conf\e[0m does not exist! Please check k3d dir! \n"
+    printf "%b" "\U1F440 Kubernetes cluster config: \e[1;34m${CLUSTER_NAME}.conf\e[0m does not exist! Please check k3d dir! \n"
     exit 1
   fi
 }
 
 function delete_k3d_cluster {
   ### Delete cluster if still exist
-  if [[ $(k3d cluster list --no-headers | grep $clustername) ]]; then
-    printf "%b" "\U26A1 Cluster: \e[1;34m$clustername\e[0m exists and will be deleted "
-    k3d cluster delete $clustername >> $k3d_log 2>&1 &
+  if [[ $(k3d cluster list --no-headers | grep $CLUSTER_NAME) ]]; then
+    printf "%b" "\U26A1 Cluster: \e[1;34m$CLUSTER_NAME\e[0m exists and will be deleted "
+    k3d cluster delete $CLUTER_NAME >> $k3d_log 2>&1 &
   else
-    printf "%b" "\U1F631 Cluster: \e[1;34m$clustername\e[0m does not exist no action taken...\n"
+    printf "%b" "\U1F631 Cluster: \e[1;34m$CLUSTER_NAME\e[0m does not exist no action taken...\n"
     exit
   fi
 }
 
 function list_k3d_cluster {
-  status=$(k3d cluster list --no-headers | grep $clustername)
+  status=$(k3d cluster list --no-headers | grep $CLUSTER_NAME)
   if [[ $status ]]; then
-    printf "%b" "\U1F44D Cluster: \e[1;34m$clustername\e[0m exist!\n"
+    printf "%b" "\U1F44D Cluster: \e[1;34m$CLUSTER_NAME\e[0m exist!\n"
     printf "%b" "\e[1;32m---------------------------------------------\e[0m\n"
-    k3d cluster list $clustername
+    k3d cluster list $CLUSTER_NAME
     printf "%b" "\e[1;32m---------------------------------------------\e[0m\n"
   else
-    printf "%b" "\U274C Cluster: \e[1;34m$clustername\e[0m does not exist!\n"
+    printf "%b" "\U274C Cluster: \e[1;34m$CLUSTER_NAME\e[0m does not exist!\n"
   
     
   fi
