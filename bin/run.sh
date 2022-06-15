9ea54272daffa2452f0cfa9f177b8dedf7633abf
johnkk84 <janusz.kujawa@gmail.com>
2022-06-15 11:05:18 +0200

    Remove progress bar + separate function cli install

--- bin/run.sh
+++ bin/run.sh
@@ -27,14 +27,10 @@ invalid_option() {
 }
 
 progress_k3d_cluster() {
-  sp='/-\|'
-  printf ' '
   while [ 1 ]; do
       k3d_progress=$(grep -i "successfully" $k3d_log)
       if [ -z "$k3d_progress" ]; then
-        printf '\b%.1s' "$sp"
-        sleep 0.2
-        sp=${sp#?}${sp%???}
+        sleep 1
       else
         if grep -q "deleted" "$k3d_log"; then
           printf "%b" "\n\U2705 Cluster: \e[1;34m$CLUSTER_NAME\e[0m sucessfully deleted\n"
@@ -49,7 +45,8 @@ progress_k3d_cluster() {
   done
 }
 
-function create_k3d_cluster {
+function install_clis {
+  # k3d
   if ! command -v k3d &> /dev/null
   then
     printf "%b" "\U1F197 No k3d cli present installing...\n"
@@ -57,7 +54,26 @@ function create_k3d_cluster {
   else
     printf "%b" "\U1F6AB k3d cli was previously installed...\n"
   fi
-  ## Create cluster if des not exist
+  
+  # kubectl
+  if ! command -v kubectl &> /dev/null
+  then
+    printf "%b" "\U1F197 No kubectl cli present installing...\n"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+    chmod +x kubectl
+    mkdir -p ~/.local/bin
+    mv ./kubectl ~/.local/bin/kubectl
+    kubectl version
+  else
+    printf "%b" "\U1F6AB kubectl cli was previously installed...\n"
+  fi
+
+}
+
+function create_k3d_cluster {
+  # Create cluster if des not exist
   if [[ $(k3d cluster list --no-headers | grep $CLUSTER_NAME) ]]; then
     printf "%b" "\U26D4 Cluster: \e[1;34m$CLUSTER_NAME\e[0m already exist!\n"
     exit
@@ -71,7 +87,7 @@ function create_k3d_cluster {
 }
 
 function delete_k3d_cluster {
-  ### Delete cluster if still exist
+  # Delete cluster if still exist
   if [[ $(k3d cluster list --no-headers | grep $CLUSTER_NAME) ]]; then
     printf "%b" "\U26A1 Cluster: \e[1;34m$CLUSTER_NAME\e[0m exists and will be deleted "
     k3d cluster delete $CLUTER_NAME >> $k3d_log 2>&1 &
@@ -108,6 +124,7 @@ while getopts "hcdl" options; do
       usage                                        
       ;;
     c)
+      install_clis
       create_k3d_cluster
       progress_k3d_cluster
       ;;
